0a35aeed53f4e1dcc4a6c4d60656bdf60bc25c29
Wojtek Porczyk <woju@invisiblethingslab.com>
2023-10-18 21:40:15 +0200

    Build using mmdebstrap
    - Builds in chroot (not in docker), and allows to run gramine-sgx-sign
      from outside of the chroot (because of plugins); the catch is that
      the two packages need to match bit for bit.
    - Requires Gramine 1.6 (not yet released as of time of this writing).
    - For now, until Gramine 1.6 is released, use unstable-* repos.
    - Update Ubuntu 20.04 (jammy) to Debian 12 (bookworm).
    - Because of that, few things shifted in manifests.
    - Retire --sgx and --sgx-key options, because:
        1) turning on/off SGX is not a global thing, user should be able to
           run any image in simulation mode at any time;
        2) gramine-direct was never working for flask anyway;
        3) in different gramine-sgx-sign plugins there may not be a --key at
           all.
    - Replace --sgx-key with {% block sign_args %} in template and
      sgx.sign_args config in scag.toml.
    - Update documentation.
    
    Co-authored-by: Mariusz Zaborski <oshogbo@invisiblethingslab.com>
    Signed-off-by: Mariusz Zaborski <oshogbo@invisiblethingslab.com>
    Signed-off-by: Wojtek Porczyk <woju@invisiblethingslab.com>

---
+++ graminescaffolding/templates/mmdebstrap-hooks/customize.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+{% block install -%}
+cp -rT {{ scag.builder.project_dir | shquote }} "$1"/app
+rm -rf "$1"/app/.scag
+cp -r {{ scag.builder.magic_dir | shquote }}/app.manifest.template "$1"/app
+
+if test -d {{ scag.builder.magic_dir | shquote }}/etc
+then
+    cp -rT {{ scag.builder.magic_dir | shquote }}/etc "$1"/usr/local/etc
+fi
+{%- endblock %}
+
+{% block manifest -%}
+chroot "$1" gramine-manifest \
+    {% block manifest_args %}{% endblock %} \
+    /app/app.manifest.template \
+    /app/app.manifest
+{%- endblock %}
+
+{% block sign -%}
+gramine-sgx-sign \
+    --date 0000-00-00 \
+    {% block sign_args %}{{ sgx.sign_args | default([]) | map('shquote') | join(' ') }}{% endblock %} \
+    --chroot "$1" \
+    --manifest "$1"/app/app.manifest \
+    --output "$1"/app/app.manifest.sgx
+{%- endblock %}
+
+{#- vim: set ft=jinja : #}
