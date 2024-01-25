cb99d9ccca88cd5b57c5597b07ec95e1f05097f8
Mariusz Zaborski <oshogbo@invisiblethingslab.com>
2024-01-25 20:02:14 +0000

    Move building to Docker
    Signed-off-by: Mariusz Zaborski <oshogbo@invisiblethingslab.com>

--- graminescaffolding/templates/mmdebstrap-hooks/customize.sh
+++ graminescaffolding/templates/mmdebstrap-hooks/customize.sh
@@ -2,26 +2,7 @@
 
 set -e
 
-{% block install -%}
-cp -rT {{ scag.builder.project_dir | shquote }} "$1"/app
-rm -rf "$1"/app/.scag
-cp -r {{ scag.builder.magic_dir | shquote }}/app.manifest.template "$1"/app
-
-if test -d {{ scag.builder.magic_dir | shquote }}/etc
-then
-    cp -rT {{ scag.builder.magic_dir | shquote }}/etc "$1"/usr/local/etc
-fi
-{%- endblock %}
-
-{% block build -%}
-{%- endblock %}
-
-{% block manifest -%}
-chroot "$1" gramine-manifest \
-    -Dpassthrough_env={{ passthrough_env | join (':') }} \
-    {% block manifest_args %}{% endblock %} \
-    /app/app.manifest.template \
-    /app/app.manifest
+{% block customize -%}
 {%- endblock %}
 
 {#- vim: set ft=jinja : #}
